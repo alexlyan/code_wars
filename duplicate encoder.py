22cb07bedf35724c2de5f3feb997336411bec963
Alex <alexlyan@yahoo.com>
2020-04-07 00:40:23 +0600

    solved 7 kyu TV remote

--- duplicate encoder.py
+++ duplicate encoder.py
@@ -1,7 +1,8 @@
 # The goal of this exercise is to convert a string to a new string where each character in the new string is
-#  "(" if that character appears only once in the original string, or ")" 
-# if that character appears more than once in the original string. Ignore capitalization 
+#  "(" if that character appears only once in the original string, or ")"
+# if that character appears more than once in the original string. Ignore capitalization
 # when determining if a character is a duplicate.
+
 
 def duplicate_encode(word):
     word = word.lower()
@@ -12,6 +13,7 @@ def duplicate_encode(word):
         else:
             result += ")"
     return result
+
 duplicate_encode("ee33er")
 
 
