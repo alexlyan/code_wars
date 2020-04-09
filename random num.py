0655b9cd3d18e02be98d213edda6037060ef3885
Alex <alexlyan@yahoo.com>
2020-04-09 09:29:04 +0600

    solved code wars problems

--- random num.py
+++ random num.py
@@ -1,16 +1,12 @@
 import random
 
-
 def rd():
-    """list random numbers """
+    '''list random numbers'''
 
-    lst = []
-    # n = int(input("Enter how diffuclt should be numbers: "))
+    # n = int(input("Enter how difficult should be numbers: "))
     for _ in range(2):
-        n = random.randint(0, 999999999)
-        lst.append(n)
-    print(lst)
-
+        n = random.randint(0, 999)
+        print(n)
 
 rd()
 
