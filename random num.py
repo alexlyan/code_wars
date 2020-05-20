94dc5809df336af94ef309741176be42175cc113
Alex <alexlyan@yahoo.com>
2020-05-20 16:21:04 +0600

    new solved problems

--- random num.py
+++ random num.py
@@ -4,9 +4,8 @@ def rd():
     '''list random numbers'''
 
     # n = int(input("Enter how difficult should be numbers: "))
-    for _ in range(2):
-        n = random.randint(0, 999)
-        print(n)
+    for _ in range(8):
+        print(random.randint(0, 999))
 
 rd()
 
